fbbd4933b9dd50b7386a933a1a85025879fa67ac
IkramIriansyah486 <ikramirian1234@gmail.com>
2024-03-13 21:40:59 +0700

    Update guided dan unguided

--- Pertemuan-1_Modul1-Tipe Data/Unguided/Unguided2.cpp
+++ Pertemuan-1_Modul1-Tipe Data/Unguided/Unguided2.cpp
@@ -1,7 +1,27 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-    
+class Lingkaran
+{
+private:
+    double jari_jari;
+
+public:
+    void setJariJari(double r)
+    {
+        jari_jari = r;
+    }
+    double hitungLuas()
+    {
+        return 3.14 * jari_jari * jari_jari;
+    }
+};
+
+int main()
+{
+    Lingkaran lingkaran1;
+    lingkaran1.setJariJari(21);
+    cout << "Luas lingkaran: " << lingkaran1.hitungLuas();
+    return 0;
 }
